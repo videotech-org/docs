23867e3c884e8d996baca646c5a4793986ea75ab
marek.wituszynski@gmail.com <marek.wituszynski@gmail.com>
2022-01-28 23:50:28 +0700

    Change repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Veedmo',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'videotech-org', // Usually your GitHub org/user name.
+  projectName: 'videotech-org.github.io', // Usually your repo name.
 
   presets: [
     [
